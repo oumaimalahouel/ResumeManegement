d2b1fd04e3de8c674c214dd4a8098d203fe9516f
oumaima <lahoueloumaima1@gmail.com>
2023-04-14 14:24:15 +0100

    initial commit

---
+++ backend/backend/Core/Context/ApplicationDbContextcs.cs
@@ -0,0 +1,30 @@
+﻿using backend.Core.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Core.Context
+{
+    public class ApplicationDbContextcs : DbContext
+    {
+        public ApplicationDbContextcs(DbContextOptions<ApplicationDbContextcs> options) : base(options)
+        {
+        
+        }
+        public DbSet<Company>companies { get; set; }
+        public DbSet<Job> jobs { get; set; }    
+        public DbSet<Candidate> candidates { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Job>()
+                .HasOne(job => job.Company)
+                .WithMany(company => company.Jobs)
+                .HasForeignKey(job => job.CompanyId);
+
+            modelBuilder.Entity<Candidate>()
+                .HasOne(candidate => candidate.Job)
+                .WithMany(job => job.candidates)
+                .HasForeignKey(candidate => candidate.JobId);
+
+        }
+    }
+}
