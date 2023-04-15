08dc4bd76dde49a86ec8802bae7ed3754a8bc69a
oumaima <lahoueloumaima1@gmail.com>
2023-04-15 09:17:41 +0100

    HttpGet: GetCandidates + HttpGet: DownloadPdfFile

--- backend/backend/Controllers/CandidateController.cs
+++ backend/backend/Controllers/CandidateController.cs
@@ -4,6 +4,7 @@ using backend.Core.Dtos.Candidate;
 using backend.Core.Entites;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers
 {
@@ -51,7 +52,33 @@ namespace backend.Controllers
 
             return Ok("Candidate Saved Successfully");
         }
+        // Read
+        [HttpGet]
+        [Route("Get")]
+        public async Task<ActionResult<IEnumerable<CandidateGetDto>>> GetCandidates()
+        {
+            var candidates = await _context.candidates.Include(c => c.Job).OrderByDescending(q => q.CreatedAt).ToListAsync();
+            var convertedCandidates = _mapper.Map<IEnumerable<CandidateGetDto>>(candidates);
 
+            return Ok(convertedCandidates);
+        }
+
+        // Read (Download Pdf File)
+        [HttpGet]
+        [Route("download/{url}")]
+        public IActionResult DownloadPdfFile(string url)
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "documents", "pdfs", url);
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound("File Not Found");
+            }
+
+            var pdfBytes = System.IO.File.ReadAllBytes(filePath);
+            var file = File(pdfBytes, "application/pdf", url);
+            return file;
+        }
 
 
     }
