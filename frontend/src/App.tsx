cadf4c2551ef90774683f4e04474fba08736b181
oumaima <lahoueloumaima1@gmail.com>
2023-04-20 13:25:38 +0100

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { useContext, lazy, Suspense } from "react";
 
 import { ThemeContext } from "./context/theme.context";
+import  Navbar  from "./components/navbar/Navbar.component";
+import { Route, Routes } from "react-router-dom";
 
 
 
@@ -13,8 +15,26 @@ const App = () => {
 
    return (
       <div className={appStyles}>
-         <h1>Navbar</h1>
-        <div className="wrapper">Routes</div>
+         <Navbar />
+         <div className="wrapper">
+            <Suspense fallback={""}>
+               <Routes>
+                  <Route path="/" element={""} />
+                  <Route path="/companies">
+                     <Route index element={""} />
+                     <Route path="add" element={""} />
+                  </Route>
+                  <Route path="/jobs">
+                     <Route index element={""} />
+                     <Route path="add" element={""} />
+                  </Route>
+                  <Route path="/candidates">
+                     <Route index element={""} />
+                     <Route path="add" element={""} />
+                  </Route>
+               </Routes>
+            </Suspense>
+         </div>
       </div>
    );
 };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./global.scss";
 import ThemeContextProvider from "./context/theme.context";
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+import { BrowserRouter } from "react-router-dom";
+import "./global.scss";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <ThemeContextProvider>
-    <App/>
-  </ThemeContextProvider>
-); 
+   <ThemeContextProvider>
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+   </ThemeContextProvider>
+);
