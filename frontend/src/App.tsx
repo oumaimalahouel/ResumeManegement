3dabaa51c8d6e35c9815d130b36e4d66d4fbfe15
oumaima <lahoueloumaima1@gmail.com>
2023-04-20 03:06:37 +0100

    Starting Frontend Project & ThemeContext

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { useContext, lazy, Suspense } from "react";
+
+import { ThemeContext } from "./context/theme.context";
+
+
+
+
+
+const App = () => {
+   const { darkMode } = useContext(ThemeContext);
+
+   const appStyles = darkMode ? "app dark" : "app";
+
+   return (
+      <div className={appStyles}>
+         <h1>Navbar</h1>
+        <div className="wrapper">Routes</div>
+      </div>
+   );
+};
+
+export default App;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./global.scss";
+import ThemeContextProvider from "./context/theme.context";
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <ThemeContextProvider>
+    <App/>
+  </ThemeContextProvider>
+); 
