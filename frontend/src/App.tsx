49dc2acf36de99dd252f3babf70d110a0a925442
oumaima <lahoueloumaima1@gmail.com>
2023-04-23 11:57:39 +0100

    Jobs Page & AddJobPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import Navbar from "./components/navbar/Navbar.component";
 import { ThemeContext } from "./context/theme.context";
 import { Routes, Route } from "react-router-dom";
 import CustomLinearProgress from "./components/custom-linear-progress/custoom-linear_progress";
+import Jobs from "./components/pages/jobs/Jobs.page";
+import AddJob from "./components/pages/jobs/AddJob.page";
 
 // Imports with Lazy loading
 const Home = lazy(() => import("./components/pages/home/Home.page"));
@@ -27,8 +29,8 @@ const App = () => {
                      <Route path="add" element={<AddCompany />} />
                   </Route>
                   <Route path="/jobs">
-                     <Route index element={""} />
-                     <Route path="add" element={""} />
+                     <Route index element={<Jobs/>} />
+                     <Route path="add" element={<AddJob/>} />
                   </Route>
                   <Route path="/candidates">
                      <Route index element={""} />
